6f2b0f4d8dc12892e0b105f83a51474fa5759fa6
Cookie Vang <vang2660@morris.umn.edu>
2020-09-17 14:54:50 -0500

    Add starting code to main file

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Please input the number of companies and people (Max of 26): ");
+        //PETER SAID IT WAS OK TO CAP AT 26
+        Scanner scanner = new Scanner(System.in);
+        int clients = scanner.nextInt();
+        //use parseInt to input the lines of a text file here
+    }
+
+    // Client can either mean the company or the people
+    public static ArrayList<T>[] hire(int client){
+        return null;
+    }
+
+    //helper method that compares two rankings
+    public static boolean compareRank(T client1, T client2, ArrayList<T>, ArrayList<T>){
+
+    }
+}
